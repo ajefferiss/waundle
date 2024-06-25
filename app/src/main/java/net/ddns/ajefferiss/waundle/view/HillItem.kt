76f03b102b751d50124be47b2ae2768ad6aff9ed
Adam Jefferiss <ajefferiss@tripadvisor.com>
2024-06-25 15:35:55 +0100

    Update hill description

--- app/src/main/java/net/ddns/ajefferiss/waundle/view/HillItem.kt
+++ app/src/main/java/net/ddns/ajefferiss/waundle/view/HillItem.kt
@@ -1,30 +1,46 @@
 package net.ddns.ajefferiss.waundle.view
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import net.ddns.ajefferiss.waundle.data.Hill
 
 @Composable
 fun HillItem(hill: Hill, onClick: () -> Unit) {
+
+    var hillDescription = ""
+    var descriptionSeparator = ""
+    if (hill.climbed != null) {
+        hillDescription += "Walked on " + hill.climbed.toString()
+        descriptionSeparator = ", "
+    }
+    hillDescription += descriptionSeparator + hill.feet + "ft, " + hill.metres + "m"
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 8.dp, start = 8.dp, end = 8.dp)
             .clickable { onClick() },
-//        elevation = CardElevation,
-//        backgroundColor = Color.White
+        elevation = CardDefaults.cardElevation(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(text = hill.name, fontWeight = FontWeight.ExtraBold)
-            Text(text = "Walked on " + hill.climbed.toString())
+            Text(text = hillDescription, fontWeight = FontWeight.SemiBold)
         }
     }
 }
