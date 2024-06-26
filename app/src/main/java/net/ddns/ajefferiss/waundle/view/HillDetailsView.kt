faaf335f2ad1edc8980f006f2d3f930689139400
Adam Jefferiss <ajefferiss@tripadvisor.com>
2024-06-26 11:13:57 +0100

    Basic Hill Details

--- app/src/main/java/net/ddns/ajefferiss/waundle/view/HillDetailsView.kt
+++ app/src/main/java/net/ddns/ajefferiss/waundle/view/HillDetailsView.kt
@@ -3,19 +3,24 @@ package net.ddns.ajefferiss.waundle.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import net.ddns.ajefferiss.waundle.R
@@ -27,6 +32,7 @@ fun HillDetailsView(
     navController: NavController
 ) {
     val snackBarHostState = remember { SnackbarHostState() }
+    val hill = viewModel.getHillById(id).collectAsState(initial = null)
 
     Scaffold(
         snackbarHost = { SnackbarHost(snackBarHostState) },
@@ -42,11 +48,25 @@ fun HillDetailsView(
             modifier = Modifier
                 .padding(it)
                 .wrapContentSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             Spacer(modifier = Modifier.height(10.dp))
-            Text(text = "Hill Details here...")
+            if (hill.value == null) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .width(64.dp)
+                        .fillMaxWidth()
+                        .align(Alignment.CenterHorizontally)
+                )
+            } else {
+                Text(
+                    text = stringResource(id = R.string.hill_desc_name) + ": ${hill.value!!.name}",
+                    fontWeight = FontWeight.SemiBold
+                )
+                Text(text = stringResource(id = R.string.hill_desc_county) + ": ${hill.value!!.county}")
+                Text(text = stringResource(id = R.string.hill_desc_classification) + ": ${hill.value!!.classification}")
+                Text(text = stringResource(id = R.string.hill_desc_height) + ": ${hill.value!!.feet} (ft), ${hill.value!!.metres} (m)")
+            }
         }
     }
 }
