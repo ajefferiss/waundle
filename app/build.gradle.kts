b7a600ded9350e6fb7a3085f5fb80cbbbf1d4644
Adam Jefferiss <ajefferiss@tripadvisor.com>
2024-08-01 15:11:01 +0100

    Add dataStore deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.datastore.preferences)
+
 
     // Test dependencies
     testImplementation(libs.junit)
