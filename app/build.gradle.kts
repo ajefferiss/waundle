65024d9e7dfc950d9d4cefe6da298e93f6d6525e
Adam Jefferiss <ajefferiss@tripadvisor.com>
2024-08-08 16:44:50 +0100

    bump build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "net.ddns.ajefferiss.waundle"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
