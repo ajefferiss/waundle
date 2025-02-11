a2c204a3eeb47c288697c5bf27f7bbaf994a0ea8
Adam Jefferiss <ajefferiss@tripadvisor.com>
2025-02-11 14:01:10 +0000

    Add empty migration

--- app/src/main/java/net/ddns/ajefferiss/waundle/Graph.kt
+++ app/src/main/java/net/ddns/ajefferiss/waundle/Graph.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Room
 import net.ddns.ajefferiss.waundle.data.HillDatabase
 import net.ddns.ajefferiss.waundle.data.HillRepository
+import net.ddns.ajefferiss.waundle.data.MIGRATION_1_2
 
 object Graph {
     lateinit var database: HillDatabase
@@ -15,6 +16,7 @@ object Graph {
     fun provide(context: Context) {
         database = Room.databaseBuilder(context, HillDatabase::class.java, "waundle.db")
             .createFromAsset("database/waundle.db")
+            .addMigrations(MIGRATION_1_2)
             .build()
     }
 }
