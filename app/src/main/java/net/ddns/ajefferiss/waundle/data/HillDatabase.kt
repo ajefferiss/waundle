c51b51442197bb38566d3d6e738136625a3ca039
Adam Jefferiss <adam.jefferiss@gmail.com>
2026-04-08 16:05:41 +0100

    Don't export schema

--- app/src/main/java/net/ddns/ajefferiss/waundle/data/HillDatabase.kt
+++ app/src/main/java/net/ddns/ajefferiss/waundle/data/HillDatabase.kt
@@ -8,7 +8,8 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(
     entities = [Hill::class, HillsMeta::class],
-    version = 2
+    version = 2,
+    exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class HillDatabase : RoomDatabase() {
